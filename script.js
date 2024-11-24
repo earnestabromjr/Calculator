19750685558ce6834d3ef2a68f7243353ce1c57d
Earnest Abrom <156559830+EAbromjr@users.noreply.github.com>
2024-11-24 13:39:13 -0600

    Added update display event listener. Trying switch statement for operate function

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 // Compute functions
 
-function add(a, b) {
+// Component vars
+const calculator = document.getElementsByClassName('calculator');
+const textDisplay = document.getElementById("display");
+const buttons= document.querySelector('.buttons')
+// textDisplay.innerText = "Calculator";
+// Calculator event listener
 
+// Update display
+buttons.addEventListener('click', (event) => {
+    textDisplay.textContent += event.target.textContent;
+});
+
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subtract(a, b) {
-
+function subtract(num1, num2) {
+    return (num1) - (num2);
 }
 
-function multiply(a, b) {
-
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divide(a, b) {
+function divide(num1, num2) {
+    return num1 / num2;
+}
 
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            if (num2 !== 0) {
+                return divide(num1, num2);
+            } else {
+                throw new Error("Cannot divide by zero");
+            }
+        default:
+            throw new Error(`Invalid operator: ${operator}`);
+    }
 }
